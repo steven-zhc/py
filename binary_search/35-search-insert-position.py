de746a7a5c7c23726af1ce433141b5ce847e3ebe
hczhang <steven.zhang031@gmail.com>
2025-09-04 08:07:23 -0500

    add 35

---
+++ binary_search/35-search-insert-position.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        if not nums:
+            return 0
+        
+        start, end = 0, len(nums) - 1
+
+        while start + 1 < end:
+            mid = (start + end) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid
+            else:
+                end = mid
+        
+        if nums[start] >= target:
+            return start
+        elif nums[end] >= target:
+            return end
+        else:
+            return end + 1
